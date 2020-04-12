f2801d6c2aaa8573eb185861b80a15eeb11b3d2a
sid-thakur47 <sidthakur258@gmail.com>
2020-04-12 17:42:50 +0530

    ADDED:Function to validate email

--- userRegistraion.sh
+++ userRegistraion.sh
@@ -2,11 +2,32 @@
 
 shopt -s extglob
 
-read -p "Enter first name:" name
-pattern="^[A-Z][a-z]{3,}$"
-	if [[ $name =~ $pattern ]]
+
+function verifyInfo() {
+	info=$1
+	pattern="^[A-Z][a-z]{3,}$"
+	if [[ $info =~ $pattern ]]
 	then
-		echo "Name is valid "
+		echo "Name is in valid format"
 	else
 		echo "Please enter valid name"
 	fi
+}
+
+function verifyEmail() {
+	read -p "Enter Email Address:" emailAdd
+		emailPattern=^"[A-Za-z0-9]{4,}([+|_|-])?([A-Za-z0-9])?@[a-z]{1,10}.[a-z]{3}.([a-z]{2})?$"
+		if [[ $emailAdd =~ $emailPattern ]]
+		then
+			echo"Email is valid"
+		else
+			echo "Please enter valid email id"
+		fi
+}
+
+read -p "Enter first name:" name
+verifyInfo $name
+read -p "Enter last name:" lastName
+verifyInfo $lastName
+verifyEmail
+
