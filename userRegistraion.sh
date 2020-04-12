bb2785756ae2f304e571b955f7ccfda32a6c315d
sid-thakur47 <sidthakur258@gmail.com>
2020-04-12 18:11:20 +0530

    ADDED:Function to validate mobile number

--- userRegistraion.sh
+++ userRegistraion.sh
@@ -2,21 +2,9 @@
 
 shopt -s extglob
 
-
-function verifyInfo() {
-	info=$1
-	pattern="^[A-Z][a-z]{3,}$"
-	if [[ $info =~ $pattern ]]
-	then
-		echo "Name is in valid format"
-	else
-		echo "Please enter valid name"
-	fi
-}
-
 function verifyEmail() {
 	read -p "Enter Email Address:" emailAdd
-		emailPattern="^[A-Za-z0-9]{4,}([+|_|-])?([A-Za-z0-9])?@[a-z]{1,10}.[a-z]{3}.([a-z]{2})?$"
+		emailPattern="^[A-Za-z0-9]{4,}([+|_|-])?([A-Za-z0-9])?@[a-z]{5}.[a-z]{3}.([a-z]{2})?$"	
 		if [[ $emailAdd =~ $emailPattern ]]
 		then
 			echo"Email is valid"
@@ -25,9 +13,32 @@ function verifyEmail() {
 		fi
 }
 
+function verifyInfo() {
+	info=$1
+	pattern="^[A-Z][a-z]{3,}$"
+		if [[ $info =~ $pattern ]]
+		then
+			echo "Name is in valid format"
+		else
+			echo "Please enter valid name"
+		fi
+}
+
+function verifyMobileNo() {
+	read -p "Enter your mobile number:" mobileNumber
+	numberPattern="^[0-9]{2}[ ][0-9]{10}$"
+		if [[ $mobileNumber =~ $numberPattern ]]
+		then
+			echo "Mobile no is valid"
+		else
+			echo "Please enter valid mobile number"
+		fi
+}
+
 read -p "Enter first name:" name
 verifyInfo $name
 read -p "Enter last name:" lastName
 verifyInfo $lastName
 verifyEmail
+verifyMobileNo
 
