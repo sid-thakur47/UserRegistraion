7339ad2279c25e93c871e0ce3c8514905f00cd1f
sid-thakur47 <sidthakur258@gmail.com>
2020-04-12 17:46:24 +0530

    MODIFIED: Regex pattern

--- userRegistraion.sh
+++ userRegistraion.sh
@@ -16,7 +16,7 @@ function verifyInfo() {
 
 function verifyEmail() {
 	read -p "Enter Email Address:" emailAdd
-		emailPattern=^"[A-Za-z0-9]{4,}([+|_|-])?([A-Za-z0-9])?@[a-z]{1,10}.[a-z]{3}.([a-z]{2})?$"
+		emailPattern="^[A-Za-z0-9]{4,}([+|_|-])?([A-Za-z0-9])?@[a-z]{1,10}.[a-z]{3}.([a-z]{2})?$"
 		if [[ $emailAdd =~ $emailPattern ]]
 		then
 			echo"Email is valid"
