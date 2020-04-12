214510ea69646f9bcc18fdbf89f3162f4fc70ef6
sid-thakur47 <sidthakur258@gmail.com>
2020-04-12 17:30:06 +0530

    ADDED:Function to validate last name

--- userRegistraion.sh
+++ userRegistraion.sh
@@ -2,11 +2,18 @@
 
 shopt -s extglob
 
+function verifyInfo() {
+	info=$1
+	pattern="^[A-Z][a-z]{3,}$"
+		if [[ $info =~ $pattern ]]
+		then
+			echo "Name is valid"
+		else
+			echo "Please enter valid name"
+		fi
+}
+
 read -p "Enter first name:" name
-pattern="^[A-Z][a-z]{3,}$"
-if [[ $name =~ $pattern ]]
-then
-	echo "Name is valid "
-else
-	echo "Please enter valid name"
-fi
+verifyInfo $name
+read -p "Enter last name:" lastName
+verifyInfo $lastName
