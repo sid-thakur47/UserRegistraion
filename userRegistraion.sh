36c92cf9ab4a48e2f5486bff4017cc5b079c89c8
sid-thakur47 <sidthakur258@gmail.com>
2020-04-14 15:28:59 +0530

    MODIFIED:Regex pattern for Password in UC8

--- userRegistraion.sh
+++ userRegistraion.sh
@@ -37,7 +37,7 @@ function verifyMobileNo() {
 function verifyPassword() {
 
 read -p "Enter your password:" password
-	passPattern="(?=.*[A-Z])(?=.*\d)(?=.*[#$^+=!*()@%&])[a-z].{8,}$"
+	passPattern="(?=.*[A-Z])(?=.*[\d])(?=.*[#$!*@&])[a-zA-Z0-9#$!*@&]{8,}$"
 		if [[ $password =~ $passPattern ]]
 		then
 			echo "Password is valid"
